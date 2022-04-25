c6dc9f597c3f271151ff016b71e820fe102e39a8
Piguave Perea Victor Hugo <victor.piguave@exodosoft.com>
2022-04-25 09:35:56 -0500

    deploy web

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
-import Script from 'next/script'
+
 import About from '../components/About'
 import Banner from '../components/Banner'
 import Contact from '../components/Contact'
 import Footer from '../components/Footer'
-import Services from '../components/Services' 
+import Services from '../components/Services'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,17 +12,6 @@ export default function Home() {
     <div className={styles.container}>
       <Head>
         <title>ExodoSoft</title>
-        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-1J2BSE1K4Q`} />
-        <Script strategy="lazyOnload" >
-          {
-            `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-1J2BSE1K4Q');
-          `
-          }
-        </Script>
       </Head>
       <Banner />
       <Services />
