c063eb8916bbe2219d78ef43d5702fdb43505632
Piguave Perea Victor Hugo <victor.piguave@exodosoft.com>
2022-04-25 09:14:28 -0500

    deploy web

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,7 @@
 import '../styles/globals.css'
-import Script from 'next/script'
+
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-1J2BSE1K4Q`} />
-      <Script strategy="lazyOnload" >
-        {
-          `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-1J2BSE1K4Q');
-          `
-        }
-      </Script>
-      <Component {...pageProps} />
-    </>
-  )
+  return <Component {...pageProps} />
 }
 
 export default MyApp
