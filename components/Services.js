2a9b8ecc9b46d5ee385bf139848995f749ddf4a8
Piguave Perea Victor Hugo <victor.piguave@exodosoft.com>
2022-04-24 22:19:00 -0500

    deploy web

--- components/Services.js
+++ components/Services.js
@@ -1,12 +1,12 @@
-function Services(){
-    return(
+function Services() {
+    return (
         <div className="min-h-screen text-white bg-black p-20  flex flex-col justify-center items-center">
             <div className="" >
                 <h2 className="lx:text-8xl  text-6xl font-bold mb-6" >Desarrollo Web</h2>
                 <p className="lx:text-7xl text-5xl  font-bold">
-                    Desarrollamos sitios web informativo, comercial blog y etc. 
-                    Implementado lo ultimo en tecnologia brindando  seguridad a 
-                    los usuarios y clientes mantenimiento de 3 meses de sitio su web <span>Next.JS, React & Vue.js</span>
+                Desarrollamos sitios web informativo, comercial blog y etc. 
+                Implementado lo último en tecnología brindando seguridad a los usuarios 
+                y clientes mantenimiento de 3 meses de sitio su web  <span>Next.JS, React & Vue.js</span>
                 </p>
             </div>
         </div>
